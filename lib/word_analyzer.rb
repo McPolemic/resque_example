5a28cc75913eef648140636dfaacbc181a9fd331
Adam Lukens <spawn968@gmail.com>
2013-03-10 03:50:57 -0400

    Removed some extraneous words in word_analyzer

--- lib/word_analyzer.rb
+++ lib/word_analyzer.rb
@@ -10,12 +10,11 @@ class WordAnalyzer
         total = word.length
         i = 0
 
-        puts "Starting analysis on #{word}"
         while i < total
             at(i, total, "Processing character #{word[i]}")
             sleep 1
             i += 1
         end
-        puts "Finished analysis on #{word}"
+        puts "Finished analysis on \"#{word}\""
     end
 end
